189c9340f74f16ff304b7d807e8f7c50fb31ddad
Sanni_Damilola <114070063+Sanni-Damilola@users.noreply.github.com>
2022-12-30 11:18:24 +0100

    Add files via upload

---
+++ twosum.ts
@@ -0,0 +1,20 @@
+// const sumup = (arr: number[], value: number) => {
+//   let res: number[] = [];
+//   let has: number[] = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let current: number = arr[i];
+//     let counter: number = value - arr[i];
+
+//     if (has.indexOf(counter) > -1) {
+//       res.push(current, counter);
+//     } else {
+//       has.push(current);
+//     }
+//   }
+// };
+
+// let array: number[] = [4, 3, 2, 5, 6, 8, 0];
+
+// console.log(sumup(array, 9));
+
